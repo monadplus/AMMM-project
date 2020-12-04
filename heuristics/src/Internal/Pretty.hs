a6f4d567b4e4f48d7f88b0ef174df523b25bd9a1
Arnau Abella <arnauabella@gmail.com>
2020-12-04 20:16:45 +0100

    pretty solutions

--- heuristics/src/Internal/Pretty.hs
+++ heuristics/src/Internal/Pretty.hs
@@ -2,10 +2,40 @@ module Internal.Pretty where
 
 -------------------------------------------
 
+import Control.Monad.IO.Class
+import Data.Coerce (coerce)
+import qualified Data.Map.Strict as Map
 import Internal.Types
+import Lens.Micro.Platform
+import Text.Printf
 
 -------------------------------------------
 
--- TODO
-printSolution :: Solution -> IO ()
-printSolution = print
+printSolution :: (MonadIO m) => Solution -> m ()
+printSolution = liftIO . putStr . prettySolution
+
+prettySolution :: Solution -> String
+prettySolution solution =
+  prettyObjectiveValue solution
+    <> Map.foldlWithKey' go mempty (coerce solution)
+  where
+    prettyObjectiveValue :: Solution -> String
+    prettyObjectiveValue = printf "Objective value %.2f\n" . computeObjectiveValue
+
+    go :: String -> City -> Assignment -> String
+    go acc c a = acc <> pretty c a
+
+    pretty :: City -> Assignment -> String
+    pretty (City _ l _) (Assignment p s) =
+      printf
+        "City %s\n* Primary facility %s\n* Secondary facility %s\n"
+        (prettyLocation l)
+        (prettyFacility p)
+        (prettyFacility s)
+
+    prettyFacility :: Facility -> String
+    prettyFacility (Facility l t _) =
+      printf "%s with cost %d" (prettyLocation l) (t ^. cost)
+
+    prettyLocation :: Location -> String
+    prettyLocation (Location x y) = printf "(%d, %d)" x y
